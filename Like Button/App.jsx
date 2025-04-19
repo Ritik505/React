092d5d953019d6ca2d98e3f1e825d0d7bd6c61df
RITIK VERMA <rv642216@gmail.com>
2025-04-19 20:02:16 +0530

    App.jsx

--- Like Button/App.jsx
+++ Like Button/App.jsx
@@ -1,7 +1,4 @@
 import "./App.css";
-import ProductTab from "./ProductTab.jsx"; // If you're using it
-import Button from "./Button.jsx";
-import Form from "./form.jsx";
 import LikeButton from "./LikeButton.jsx";
 
 function App() {
